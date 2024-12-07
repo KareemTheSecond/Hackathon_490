fdcde393a1264472c1ce9f17e17a2ada2d75f60c
KareemTheSecond <kareemrhassani@gmail.com>
2024-12-07 15:44:17 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ from DiscreteBasicExtraction import ExtractFromExcelDiscrete
 from PCA_Methods import ElbowBestCluster,BicsBestCluster,PCA_PlotClusters
 from AutoEncoder import DimentionalityReductionCluster
 
-ExcelPath = "2024_PersonalityTraits_SurveyData.xlsx"
+ExcelPath = "FILEPATH"
 df3 = ExtractFromExcelDiscrete(ExcelPath ,False ) #The False means you want to use the defualt scaling  (recommended....) 
 print(df3.head())
 
