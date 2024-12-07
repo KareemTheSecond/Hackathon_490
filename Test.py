6b70011bb229cc361d0977e70f72376f088a676f
KareemTheSecond <kareemrhassani@gmail.com>
2024-12-07 15:12:05 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,7 @@
 from DiscreteBasicExtraction import ExtractFromExcelDiscrete
 
-ExcelPath = ExcelPath = "2024_PersonalityTraits_SurveyData.xlsx"
+ExcelPath = "2024_PersonalityTraits_SurveyData.xlsx"
 df3 = ExtractFromExcelDiscrete(ExcelPath ,False ) #The False means you want to use the defualt scaling  (recommended....) 
+print(df3.head())
+
 
