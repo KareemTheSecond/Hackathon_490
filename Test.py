2f6eba73eea450432d079123b271f373ca792b1c
KareemTheSecond <kareemrhassani@gmail.com>
2024-12-07 15:30:26 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,17 @@
 from DiscreteBasicExtraction import ExtractFromExcelDiscrete
+from PCA_Methods import ElbowBestCluster,BicsBestCluster,PCA_PlotClusters
+from AutoEncoder import DimentionalityReductionCluster
 
 ExcelPath = "2024_PersonalityTraits_SurveyData.xlsx"
 df3 = ExtractFromExcelDiscrete(ExcelPath ,False ) #The False means you want to use the defualt scaling  (recommended....) 
 print(df3.head())
+
+ElbowBestCluster(df3) #Visual Aid to decide
+BicsBestCluster(df3) #Visual Aid to decide
+PCA_PlotClusters(df3, 3) 
+PCA_PlotClusters(df3, 5)
+IdealNumberOfCluster = PCA_PlotClusters(df3, 10) 
+print(IdealNumberOfCluster)
+DimentionalityReductionCluster(df3)
 
 
